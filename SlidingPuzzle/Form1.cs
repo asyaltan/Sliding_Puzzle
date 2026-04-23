2342efe0b8fca5d88c10da241a5bedbce9d85a88
asyaltan <asyaltan@gmail.com>
2026-04-23 23:16:14 +0300

    Add files via upload

--- SlidingPuzzle/Form1.cs
+++ SlidingPuzzle/Form1.cs
@@ -1,176 +1,239 @@
-using Microsoft.VisualBasic.Devices;
-using System.Diagnostics;
-using System.Numerics;
+using System;
+using System.IO;
 using System.Drawing;
+using System.Numerics;
+using System.Windows.Forms;
 
 namespace SlidingPuzzle
 {
     public partial class Form1 : Form
     {
-        #region Gloabl Variables
-
-        public const double PI = Math.PI;
-
-        public Point[] pts;
-        public PictureBox[] pictureBoxes;
-        int emptyIndex = 8;
-        public Vector2 downPos, upPos;
+        #region Değişkenler
+        public Label lblTime = null!;
+        public Label lblMoves = null!;
+        public Point[] pts = null!;
+        public PictureBox[] pictureBoxes = null!;
         public int[] indexes = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-        public int index = 0;
-        public string baseUrl  = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "baseImage2.jpeg");
+        public int emptyIndex = 8;
+        public Vector2 downPos, upPos;
+        public int index;
+        public string baseUrl = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "baseImage2.jpeg");
 
+        public int moveCount = 0;
+        public bool isGameOver = false;
+        public bool isAnimating = false;
+        public Image? lastPiece;
+
+        private System.Windows.Forms.Timer gameTimer = null!;
+        private int secondsElapsed = 0;
+        private System.Windows.Forms.Timer animTimer = null!;
+        private PictureBox? movingBox;
+        private Point targetPos;
+        private const int animSpeed = 25;
         #endregion
 
         public Form1()
         {
             InitializeComponent();
 
-            pts = new Point[] { pictureBox0.Location, pictureBox1.Location, pictureBox2.Location, pictureBox3.Location, pictureBox4.Location, pictureBox5.Location, pictureBox6.Location, pictureBox7.Location, pictureBox8.Location };
+            // 1. Arayüz elemanlarını ve sistemleri hazırla
+            SetupGameSystem();
+
+            // 2. Olayları kodla bağla
+            LinkEventsDynamically();
+
+            // 3. Başlangıçta varsayılan resmi yükle
+            this.Load += (s, e) => {
+                if (File.Exists(baseUrl))
+                {
+                    CutImage(baseUrl);
+                    Shuffle();
+                }
+                else
+                {
+                    MessageBox.Show("Masaüstünde 'baseImage2.jpeg' bulunamadı.\nLütfen 'Resim Seç' butonuyla bir resim yükleyin.");
+                }
+            };
+        }
+
+        private void SetupGameSystem()
+        {
             pictureBoxes = new PictureBox[] { pictureBox0, pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8 };
-            CutImage(baseUrl);
-            Shuffle();
-        }
+            pts = new Point[] { pictureBox0.Location, pictureBox1.Location, pictureBox2.Location, pictureBox3.Location, pictureBox4.Location, pictureBox5.Location, pictureBox6.Location, pictureBox7.Location, pictureBox8.Location };
 
-        #region Mouse Events
-        private void box_MouseDown(object sender, MouseEventArgs e)
-        {
-            downPos = new Vector2(e.X, e.Y);
-            index = Array.IndexOf(indexes, Array.IndexOf(pictureBoxes, sender));
+            int maxBottom = 0;
+            foreach (var pb in pictureBoxes)
+                if (pb.Bottom > maxBottom) maxBottom = pb.Bottom;
 
-        }
-        private void box_MouseUp(object sender, MouseEventArgs e)
-        {
-            upPos = new Vector2(e.X, e.Y);
-
-            Direction direction = getMouseDirection(downPos, upPos);
-            //MessageBox.Show(direction.ToString());
-
-            swap(index, direction);
-
-        }
-        #endregion
-
-        #region Helper Functions
-        Direction getMouseDirection(Vector2 down, Vector2 up)
-        {
-            Vector2 dir = up - down;
-            var angle = Math.Atan2(dir.Y, dir.X);// Math.Acos((up.X * down.X + up.Y * down.Y) / Math.Sqrt((up.X * up.X + up.Y + up.Y) * (down.X * down.X + down.Y * down.Y)));
-            var sect = PI / 4;
-
-            if (angle < 0)
-                angle += 2 * PI;
-
-            if (angle <= 1 * sect || angle > 7 * sect)
+            lblTime = new Label
             {
-                return Direction.Right;
-            }
+                Location = new Point(pictureBox0.Left, maxBottom + 20), 
+                AutoSize = true,
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                ForeColor = Color.DarkRed,
+                Text = "Süre: 0 sn"
+            };
 
-            if (angle <= 3 * sect)
+            lblMoves = new Label
             {
-                return Direction.Down;
-            }
+                Location = new Point(pictureBox0.Left + 200, maxBottom + 20),
+                AutoSize = true,
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                ForeColor = Color.DarkBlue,
+                Text = "Hamle: 0"
+            };
 
-            if (angle <= 5 * sect)
-            {
-                return Direction.Left;
-            }
+            this.Controls.Add(lblTime);
+            this.Controls.Add(lblMoves);
+            lblTime.BringToFront();
+            lblMoves.BringToFront();
 
-            return Direction.Up;
+            this.Height = lblTime.Bottom + 100;
+            this.Width = Math.Max(this.Width, 600);
+
+            gameTimer = new System.Windows.Forms.Timer { Interval = 1000 };
+            gameTimer.Tick += (s, e) => { secondsElapsed++; lblTime.Text = $"Süre: {secondsElapsed} sn"; };
+            animTimer = new System.Windows.Forms.Timer { Interval = 10 };
+            animTimer.Tick += AnimTimer_Tick!;
         }
 
-        void swap(int index, Direction direction)
+        private void LinkEventsDynamically()
         {
+            foreach (var pb in pictureBoxes)
+            {
+                pb.MouseDown += box_MouseDown!;
+                pb.MouseUp += box_MouseUp!;
+            }
+        }
+
+        private void AnimTimer_Tick(object sender, EventArgs e)
+        {
+            if (movingBox == null) return;
+            int dx = targetPos.X - movingBox.Left;
+            int dy = targetPos.Y - movingBox.Top;
+
+            if (Math.Abs(dx) <= animSpeed && Math.Abs(dy) <= animSpeed)
+            {
+                movingBox.Location = targetPos;
+                animTimer.Stop();
+                isAnimating = false;
+                if (CheckWin()) FinishGame();
+            }
+            else
+            {
+                movingBox.Left += Math.Sign(dx) * animSpeed;
+                movingBox.Top += Math.Sign(dy) * animSpeed;
+            }
+        }
+
+        void swap(int index, Direction direction, bool animate = true)
+        {
+            if (isGameOver || isAnimating) return;
             int newIndex = -1;
-
             switch (direction)
             {
-                case Direction.Right:
-                    if (index % 3 != 2)
-                        newIndex = index + 1;
-                    break;
-                case Direction.Down:
-                    if (index < 6)
-                        newIndex = index + 3;
-                    break;
-                case Direction.Left:
-                    if (index % 3 != 0)
-                        newIndex = index - 1;
-                    break;
-                case Direction.Up:
-                    if (index > 2)
-                        newIndex = index - 3;
-                    break;
+                case Direction.Right: if (index % 3 != 2) newIndex = index + 1; break;
+                case Direction.Down: if (index < 6) newIndex = index + 3; break;
+                case Direction.Left: if (index % 3 != 0) newIndex = index - 1; break;
+                case Direction.Up: if (index > 2) newIndex = index - 3; break;
             }
 
             if (newIndex == emptyIndex)
             {
-                emptyIndex = index;
+                if (!gameTimer.Enabled && animate) gameTimer.Start();
+                movingBox = pictureBoxes[indexes[index]];
+                targetPos = pts[newIndex];
 
                 int temp = indexes[index];
                 indexes[index] = indexes[newIndex];
                 indexes[newIndex] = temp;
+                emptyIndex = index;
 
-                pictureBoxes[indexes[index]].Location = pts[index];
-                pictureBoxes[indexes[newIndex]].Location = pts[newIndex];
+                if (animate)
+                {
+                    pictureBoxes[indexes[index]].Location = pts[index];
+                    isAnimating = true;
+                    animTimer.Start();
+                    moveCount++;
+                    lblMoves.Text = $"Hamle: {moveCount}";
+                }
+                else
+                {
+                    if (movingBox != null) movingBox.Location = targetPos;
+                    pictureBoxes[indexes[index]].Location = pts[index];
+                }
             }
-
-            //MessageBox.Show(string.Join(", ", pictureBoxes));
         }
 
-        void CutImage(string url)
+        bool CheckWin()
         {
-            Bitmap original = new Bitmap(url);
+            for (int i = 0; i < indexes.Length; i++)
+                if (indexes[i] != i) return false;
+            return true;
+        }
 
-            int h = original.Height / 3;
-            int w = original.Width / 3;
-            
-            Rectangle section0 = new Rectangle(0, 0, w ,h);
-            Rectangle section1 = new Rectangle(w, 0, w, h);
-            Rectangle section2 = new Rectangle(2*w, 0, w, h);
-            Rectangle section3 = new Rectangle(0, h, w, h);
-            Rectangle section4 = new Rectangle(w, h, w, h);
-            Rectangle section5 = new Rectangle(2*w, h, w, h);
-            Rectangle section6 = new Rectangle(0, 2*h, w, h);
-            Rectangle section7 = new Rectangle(w, 2*h, w, h);
-            
-            List<int> pieces = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
-            
-            pictureBoxes[0].Image = original.Clone(section0, original.PixelFormat);
-            pictureBoxes[1].Image = original.Clone(section1, original.PixelFormat);
-            pictureBoxes[2].Image = original.Clone(section2, original.PixelFormat);
-            pictureBoxes[3].Image = original.Clone(section3, original.PixelFormat);
-            pictureBoxes[4].Image = original.Clone(section4, original.PixelFormat);
-            pictureBoxes[5].Image = original.Clone(section5, original.PixelFormat);
-            pictureBoxes[6].Image = original.Clone(section6, original.PixelFormat);
-            pictureBoxes[7].Image = original.Clone(section7, original.PixelFormat);
-            
+        void FinishGame()
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            pictureBox8.Image = lastPiece;
+            MessageBox.Show($"Tebrikler! {moveCount} hamle ve {secondsElapsed} saniyede bitirdin!");
         }
 
         void Shuffle()
         {
             Random rand = new Random();
-            for (int i = 0; i < 5000; i++)
-            {
-                int r = rand.Next(4);
-                swap(rand.Next(8), (Direction)r);
-            }
+            for (int i = 0; i < 400; i++)
+                swap(rand.Next(9), (Direction)rand.Next(4), false);
+            moveCount = 0; secondsElapsed = 0; isGameOver = false;
+            lblMoves.Text = "Hamle: 0"; lblTime.Text = "Süre: 0 sn";
         }
 
-        #endregion
+        private void box_MouseDown(object sender, MouseEventArgs e)
+        {
+            downPos = new Vector2(e.X, e.Y);
+            index = Array.IndexOf(indexes, Array.IndexOf(pictureBoxes, (PictureBox)sender));
+        }
+
+        private void box_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isAnimating) return;
+            upPos = new Vector2(e.X, e.Y);
+            if (Vector2.Distance(downPos, upPos) < 10) return;
+            Vector2 dir = upPos - downPos;
+            double angle = Math.Atan2(dir.Y, dir.X);
+            if (angle < 0) angle += 2 * Math.PI;
+            double sect = Math.PI / 4;
+            Direction d = (angle <= 1 * sect || angle > 7 * sect) ? Direction.Right : (angle <= 3 * sect) ? Direction.Down : (angle <= 5 * sect) ? Direction.Left : Direction.Up;
+            swap(index, d);
+        }
+
+        void CutImage(string url)
+        {
+            if (!File.Exists(url)) return;
+            Bitmap original = new Bitmap(url);
+            int w = original.Width / 3, h = original.Height / 3;
+            for (int i = 0; i < 8; i++)
+            {
+                Rectangle rect = new Rectangle((i % 3) * w, (i / 3) * h, w, h);
+                pictureBoxes[i].Image = original.Clone(rect, original.PixelFormat);
+            }
+            lastPiece = original.Clone(new Rectangle(2 * w, 2 * h, w, h), original.PixelFormat);
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            baseUrl = urlBox.Text;
-            
-            CutImage(baseUrl);
+            using (OpenFileDialog ofd = new OpenFileDialog { Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    baseUrl = ofd.FileName;
+                    CutImage(baseUrl);
+                    Shuffle();
+                }
+            }
         }
 
-        enum Direction
-        {
-            Right,
-            Up,
-            Left,
-            Down
-        }
+        public enum Direction { Right, Up, Left, Down }
     }
 }
